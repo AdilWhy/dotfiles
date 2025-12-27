73d4fc056f4e0f97ff7bcaaec8cfb27f2e798523
AdilWhy <adil270120@gmail.com>
2025-12-27 18:04:46 +0500

    fix: change vars to use upper case

--- setup_env.sh
+++ setup_env.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# 1. Install oh my zsh
-if [ ! -d "$home/.oh-my-zsh" ]; then
-    echo "installing oh My Zsh..."
-    sh -c "$(curl -fssl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# 1. Install Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
-    echo "oh my zsh is already installed."
+    echo "Oh My Zsh is already installed."
 fi
 
-ZSH_CUSTom=${zsh_custom:-$HOME/.oh-my-zsh/custom}
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-# 2. Install zsh plugins
-declare -a plugins=(
+# 2. Install ZSH Plugins
+declare -A PLUGINS=(
     ["fzf-tab"]="https://github.com/Aloxaf/fzf-tab"
     ["zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions"
     ["fast-syntax-highlighting"]="https://github.com/zdharma-continuum/fast-syntax-highlighting"
 )
 
-for plugin in "${!pluginS[@]}"; do
-    if [ ! -d "$zsh_custOM/plugins/$plugin" ]; then
-        echo "installing $plugin..."
-        git clone "${pluGINS[$plugin]}" "$ZSH_CUSTOM/plugins/$plugin"
+for plugin in "${!PLUGINS[@]}"; do
+    if [ ! -d "$ZSH_CUSTOM/plugins/$plugin" ]; then
+        echo "Installing $plugin..."
+        git clone "${PLUGINS[$plugin]}" "$ZSH_CUSTOM/plugins/$plugin"
     else
-        echo "plugin $plugin already exists."
+        echo "Plugin $plugin already exists."
     fi
 done
 
